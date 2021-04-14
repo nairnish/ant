af1674349237e5cd4fca113335ba75cdef680c2c
nn3063 <nn3063@g.rit.edu>
2021-04-13 21:06:09 -0400

    Corrected: Replaced for loop by a foreach loop

--- src/main/org/apache/tools/ant/Target.java
+++ src/main/org/apache/tools/ant/Target.java
@@ -424,7 +424,6 @@ public class Target implements TaskContainer {
      * @see #setIf(String)
      * @see #setUnless(String)
      */
-//    FIXME: Replace for loop by a foreach loop
     public void execute() throws BuildException {
         if (ifCondition != null && !ifCondition.eval()) {
             project.log(this, "Skipped because property '" + project.replaceProperties(ifString)
@@ -443,8 +442,7 @@ public class Target implements TaskContainer {
             // also account for growing target children
             // do not optimize this loop by replacing children.size() by a variable
             // as children can be added dynamically as in RhinoScriptTest where a target is adding work for itself
-            for (int i = 0; i < children.size(); i++) {
-                Object o = children.get(i);
+            for (Object o : children) {
                 if (o instanceof Task) {
                     Task task = (Task) o;
                     task.perform();
