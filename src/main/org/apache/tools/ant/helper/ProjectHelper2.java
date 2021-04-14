e8a4c3e2afa2032655a068599981ccc7cfcdaaf4
nn3063 <nn3063@g.rit.edu>
2021-04-13 21:16:34 -0400

    Corrected: Replaced String.ValueOf(String) with String.ValueOf(char)

--- src/main/org/apache/tools/ant/helper/ProjectHelper2.java
+++ src/main/org/apache/tools/ant/helper/ProjectHelper2.java
@@ -1130,7 +1130,6 @@ public class ProjectHelper2 extends ProjectHelper {
          * @exception SAXParseException in case of error (not thrown in
          *                              this implementation)
          */
-//        TODO: Replace String.indexOf(String) with String.indexOf(char) for a single character
         @Override
         public void onStartElement(String uri, String tag, String qname, Attributes attrs,
                                    AntXMLContext context) throws SAXParseException {
@@ -1185,7 +1184,7 @@ public class ProjectHelper2 extends ProjectHelper {
                     || (ANT_CORE_URI.equals(attrUri)
                         && ANT_TYPE.equals(attrs.getLocalName(i)))) {
                     name = ANT_TYPE;
-                    int index = value.indexOf(":");
+                    int index = value.indexOf(':');
                     if (index >= 0) {
                         String prefix = value.substring(0, index);
                         String mappedUri = context.getPrefixMapping(prefix);
